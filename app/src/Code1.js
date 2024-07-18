b8b58cf52b991f6bd3bc54c719fa2a4164c36f25
aniket2727 <aniketkadam5950@gmail.com>
2024-07-18 15:48:49 +0530

    add code

---
+++ app/src/Code1.js
@@ -0,0 +1,38 @@
+
+
+
+// code is writtten by aniket kadam
+// rest and spread operator
+
+import React from 'react'
+import { useState } from 'react'
+
+
+const Code1=()=>{
+    
+
+
+    // eslint-disable-next-line no-unused-vars
+    const [data,setdata]=useState([1,2,3,4,5])
+
+    // rest oprators
+    function handlerest(...a){
+        console.log(a)
+        
+
+        // spread operators
+        const b=[...data]
+        console.log("the value of b is",b)
+    }
+
+    handlerest(10,20,10);
+   
+    return(
+        <div>
+          <h1>rest and spred operators</h1>
+        </div>
+    )
+}
+
+
+export default Code1;
