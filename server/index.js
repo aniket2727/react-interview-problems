fd6d3c1f5861488eb54c341fc9f635a1dcf26338
aniket2727 <aniketkadam5950@gmail.com>
2024-07-28 15:54:18 +0530

    add code

--- server/index.js
+++ server/index.js
@@ -48,14 +48,48 @@ fs.readdir('.',(error,files)=>{
 // event emitters
 const EventEmitter = require('events');
 const emitter=new EventEmitter();
-
-
 emitter.on('greet',(name)=>{
     console.log(`Hello, ${name}!`);
 })
-
-
 emitter.emit('greet','aniket');
+
+
+
+// // read json file and write down code
+// const filePath='data.json';
+
+// // Step 1: Read the JSON file
+// fs.readFile(filePath, 'utf8', (err, data) => {
+//     if (err) {
+//         console.error('Error reading file:', err);
+//         return;
+//     }
+
+//     // Step 2: Parse the JSON content
+//     let jsonData;
+//     try {
+//         jsonData = JSON.parse(data);
+//     } catch (parseErr) {
+//         console.error('Error parsing JSON:', parseErr);
+//         return;
+//     }
+
+//     // Step 3: Edit the object
+//     jsonData.name = 'Ergonomic Wireless Mouse'; // Example modification
+
+//     // Step 4: Stringify the object
+//     const updatedData = JSON.stringify(jsonData, null, 2);
+
+//     // Step 5: Write the JSON file
+//     fs.writeFile(filePath, updatedData, 'utf8', (writeErr) => {
+//         if (writeErr) {
+//             console.error('Error writing file:', writeErr);
+//             return;
+//         }
+//         console.log('File successfully updated');
+//     });
+//});
+
 
 const port=9009;
 httpServer.listen(port,()=>{
