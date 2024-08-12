7aba39a80b459e03b426e9ade70fc2c692d7f26a
aniket2727 <aniketkadam5950@gmail.com>
2024-08-12 15:54:12 +0530

    add all code here

--- app/src/Code48.js
+++ app/src/Code48.js
@@ -8,18 +8,28 @@ import React from "react";
 const Simpleproblems=()=>{
 
     const data=[1,2,3,4,5];
-    for(let i  of data){
-        console.log(i)
-    }
+    // for(let i  of data){
+    //     console.log(i)
+    // }
 
-    const obj={
-        name:"aniket",
-        jobrole:"full stack developer"
-    }
+    // const obj={
+    //     name:"aniket",
+    //     jobrole:"full stack developer"
+    // }
 
-    for(let i in obj){
-        console.log(i,obj[i])
-    }
+    // for(let i in obj){
+    //     console.log(i,obj[i])
+    // }
+     
+    // slice is used for selection of data
+    let result=data.slice(1,3);
+    console.log(result);
+    console.log("the original data ",data)
+    
+    // splice is used for removing the elements from the data 
+    let result2=data.splice(1,2);
+    console.log("the result 2",result2);
+    console.log("original data",data);
     return(
         <div>
            <h1>this is the problems</h1>
