8152874d5f271a2f306850d23944d94f834483b8
aniket2727 <aniketkadam5950@gmail.com>
2024-08-13 11:51:53 +0530

    add code

--- app/src/Code61.js
+++ app/src/Code61.js
@@ -1,25 +1,28 @@
-
-
-// code is written by aniket kadam
-// avoid useEffect for a first rendering
-
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { useEffect } from 'react';
 
-const Avoiduseeffect=()=>{
-    const [count,setcount]=useState(0);
+const Avoiduseeffect = () => {
+    const [count, setCount] = useState(0);
+    
+    const useref1 = useRef(false);
 
-    return(
+    useEffect(() => {
+        if (!useref1.current) {
+            useref1.current = true; // Set the ref to true after the first render
+            return;
+        }
+
+        console.log("useEffect avoided");
+
+    }, [count]) // useEffect dependency on 'count'
+
+    return (
         <div>
-           <h1>avoid useeffect</h1>
-            <h1 style={{color:'red'}}>{count}</h1>
-           <button style={{backgroundColor:'green'}} onClick={()=>setcount(count+1)}>Add</button>
+            <h1>avoid useEffect</h1>
+            <h1 style={{ color: 'red' }}>{count}</h1>
+            <button style={{ backgroundColor: 'green' }} onClick={() => setCount(count + 1)}>Add</button>
         </div>
-    )
+    );
 };
 
-
-
 export default Avoiduseeffect;
-
-
