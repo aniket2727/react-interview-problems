01947edfabd413400e26a6ef7b6047a8d0e79798
aniket2727 <aniketkadam5950@gmail.com>
2024-10-02 20:25:42 +0530

    add code

--- app/src/Code113.js
+++ app/src/Code113.js
@@ -1,28 +1,38 @@
-
-
 // code is written by aniket kadam
 
 import { useState } from "react";
 
+const Experiments = () => {
+    // Corrected: Only one argument for useState
+    const [data] = useState({ 'name': 'aniket' });
 
-const  Experiments=()=>{
-    const [data]=useState({'name':'aniket'},{
-        'name':'vijay'
-    })
-    return(
+    const user = {
+        name: 'Aniket',
+        age: 25,
+        profession: 'Developer'
+    };
+    
+    // Using Object.entries to log the first key-value pair
+    const entries = Object.entries(user);
+    console.log("the name is ", entries[0][0]);
+
+    // Iterating through key-value pairs of the user object
+    Object.entries(user).forEach(([key, value]) => {
+        console.log(`${key}: ${value}`);
+    });
+    
+    return (
         <div>
-            <h1>this is experiment videos</h1>
+            <h1>This is experiment videos</h1>
             {
-               Object.entries(data).map(([key,value],index)=>(
-                   <div key={index}>
-                      <h1>{value}</h1>
-                   </div>
-               ))
+                Object.entries(data).map(([key, value], index) => (
+                    <div key={index}>
+                        <h1>{value}</h1>
+                    </div>
+                ))
             }
         </div>
-    )
+    );
 }
-
-
 
 export default Experiments;
