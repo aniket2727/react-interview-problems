bb3bd5c9b83797cb1fe4178501bbf43cb02d3b61
aniket2727 <aniketkadam5950@gmail.com>
2024-10-02 15:09:27 +0530

    add code

--- app/src/Code111.js
+++ app/src/Code111.js
@@ -1,6 +1,7 @@
 // code is written by Aniket Kadam
 
 const PromisesUnderstanding = () => {
+  
     async function handlePromises() {
       return new Promise(async (resolve, reject) => {
         try {
@@ -17,12 +18,44 @@ const PromisesUnderstanding = () => {
       });
     }
   
+    function handleSecond() {
+      return new Promise((resolve, reject) => {
+        try {
+          fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then((response) => {
+              if (response.ok) {
+                return response.json(); // Parse the response as JSON
+              } else {
+                reject('Error in fetching data');
+              }
+            })
+            .then((data) => {
+              resolve(data); // Resolve with the parsed data
+            })
+            .catch((error) => {
+              reject('Internal server error');
+            });
+        } catch (error) {
+          reject('Internal server error');
+        }
+      });
+    }
+  
+    // Call the promises and handle them
     handlePromises()
       .then((data) => {
-        console.log("The result is", data);
+        console.log("The result from handlePromises is", data);
       })
       .catch((error) => {
         console.error(error);
+      });
+  
+    handleSecond()
+      .then((data) => {
+        console.log("The data from handleSecond is", data);
+      })
+      .catch((error) => {
+        console.log("The error is", error);
       });
   
     return (
