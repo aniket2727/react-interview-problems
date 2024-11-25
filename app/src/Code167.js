555ac22ec6300f4fd534fbd38dbf673df03ecb3f
aniket2727 <aniketkadam5950@gmail.com>
2024-11-25 11:43:45 +0530

    add code

--- app/src/Code167.js
+++ app/src/Code167.js
@@ -18,6 +18,22 @@ const Code167=()=>{
         j--;  
     }
     console.log(a)
+
+   const data='aniket';
+   const arr=data.split('');
+   console.log(arr);
+    let k=0;
+    let m=arr.length-1;
+    while(k<m){
+        let temp='';
+        temp=arr[k];
+        arr[k]=arr[m];
+        arr[m]=temp;
+        k++;
+        m--;  
+    };
+    console.log(arr)
+
     return(
         <div>
             <h1>reverse the array</h1>
