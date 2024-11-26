9fa4469d62b99d532f9c489f1a1e53d767e83777
aniket2727 <aniketkadam5950@gmail.com>
2024-11-26 12:17:47 +0530

    add code

--- app/src/Code177.js
+++ app/src/Code177.js
@@ -8,12 +8,20 @@ import { useState } from "react";
 const Code177=()=>{
     const[data,setdata]=useState({1:"aniket",2:"vijay",3:'kadam'})
     console.log(data);
-    for(let i in data){
-        console.log(i,data[i])
-    }
+    // for(let i in data){
+    //     console.log(i,data[i])
+    // }
+    
     return(
         <div>
             <h1>tabs Component</h1>
+            {
+                data.map((item,index)=>(
+                    <div key={index}>
+                        <h1>{item.key}</h1>
+                    </div>    
+                ))
+            }
         </div>
     )
 };
