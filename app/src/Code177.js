45ac8cce9c1578214b101fe74f9bed421f6d58ae
aniket2727 <aniketkadam5950@gmail.com>
2024-11-26 12:27:31 +0530

    add code

--- app/src/Code177.js
+++ app/src/Code177.js
@@ -1,30 +1,38 @@
 
 
-//tabs component
-
 import { useState } from "react";
 
-
-const Code177=()=>{
-    const[data,setdata]=useState({1:"aniket",2:"vijay",3:'kadam'})
+const Code177 = () => {
+    const [data, setData] = useState([{ roll:1, name: 'aniket' }, {roll:2, name: 'kadam' }]);
     console.log(data);
-    // for(let i in data){
-    //     console.log(i,data[i])
-    // }
-    
-    return(
+    const[selected,setselected]=useState([]);
+
+    const handleClick=(item)=>{
+        console.log("input",item);
+          if(selected.includes(item)){
+            const result=selected.filter((a)=>a!==item);
+            setselected(result);
+          }else{
+                setselected([...selected,item])
+          }
+
+    }
+
+    return (
         <div>
-            <h1>tabs Component</h1>
+            <h1>Tabs Component</h1>
             {
-                data.map((item,index)=>(
+                data.map((item, index) => (
                     <div key={index}>
-                        <h1>{item.key}</h1>
-                    </div>    
+                        <h1 onClick={()=>handleClick(item.roll)}>{item.roll}</h1>
+                        {
+                            selected.includes(item.roll) ?<div>{item.name}</div>:null
+                        }
+                    </div>
                 ))
             }
         </div>
-    )
+    );
 };
-
 
 export default Code177;
