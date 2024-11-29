035e54d6c05377ccace87c2ddd2f99fa835be3c0
aniket2727 <aniketkadam5950@gmail.com>
2024-11-29 19:44:58 +0530

    add code

---
+++ app/src/Code205.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+//basic code for higher order components
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default (WrappedComponent) => {
+  const hocComponent = ({ ...props }) => <WrappedComponent {...props} />
+
+  hocComponent.propTypes = {}
+
+  return hocComponent
+}
