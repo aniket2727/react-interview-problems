817000fe012fda49da601bff3bf34de1c55ae86d
aniket2727 <aniketkadam5950@gmail.com>
2024-11-29 16:13:54 +0530

    add code

--- app/src/Code201.js
+++ app/src/Code201.js
@@ -1,9 +1,34 @@
 //  code is written by aniket kadam
+import React from 'react';
+import { useState } from 'react';
 
-const Code201=()=>{
-    return(
+const Code201 = () => {
+
+    const [data, setdata] = useState([
+        { name: 'aniket', age: 10 },
+        { name: 'vijay', age: 20 },
+        { name: 'kadam', age: 40 },
+        { name: 'sanket', age: 100 }]);
+
+
+        const[selected,setselected]=useState('');
+        console.log("selcted ",selected)
+    return (
         <div>
             <h1>selctions</h1>
+            <h1>{selected}</h1>
+            <div>
+                <h1>Selections</h1>
+                <select onChange={(e)=>setselected(e.target.value)}>
+                    {data.map((item, index) =>
+                        item.age > 20 ? (
+                            <option style={{ backgroundColor: 'red' }} key={index}>
+                                {item.name}
+                            </option>
+                        ) : null
+                    )}
+                </select>
+            </div>
         </div>
     )
 };
