cd1f378bf71ae273fccad6fd8261e202b0158d4c
aniket2727 <aniketkadam5950@gmail.com>
2024-12-09 18:05:57 +0530

    add code

--- app/src/Code213.js
+++ app/src/Code213.js
@@ -1,10 +1,44 @@
+// faq
+import React from 'react';
+import { useState } from 'react';
+
 
 const Code213 = () => {
-  return (
-    <div>
-      <h1>code</h1>
-    </div>
-  )
-}
 
-export default Code213
+    const [data, setdata] = useState([
+        {
+            question: 'a',
+            answer: 'b',
+            state: false
+        }, {
+            question: 'c',
+            answer: 'd', state: false
+        }, {
+            question: 'e',
+            answer: 'f', state: false
+        }
+    ]);
+
+    const handleClick = (index1) => {
+               setdata((prev)=>prev.map((item,index)=>(
+                   index===index1?{...item,state:!item.state}:item
+               )))
+    }
+    return (
+        <div>
+            {
+                data &&
+                data.map((item, index) => (
+                    <div key={index}>
+                        <h1 onClick={()=>handleClick(index)}>{item.question}</h1>
+                        {
+                            item.state ?<div>{item.answer}</div>:null
+                        }
+                    </div>
+                ))
+            }
+        </div>
+    )
+};
+
+export default Code213;
