48462149a8454edd19a17a6a35e5043f01dccef8
aniket2727 <aniketkadam5950@gmail.com>
2024-12-09 15:47:59 +0530

    add code

---
+++ app/src/Code212.js
@@ -0,0 +1,23 @@
+//countdonw approch
+
+
+import React from 'react';
+import {useState} from 'react'
+
+
+
+const Code212=()=>{
+    return(
+        <div>
+            <h1>counter</h1>
+        </div>
+    )
+};
+
+
+
+export default Code212;
+
+
+
+
