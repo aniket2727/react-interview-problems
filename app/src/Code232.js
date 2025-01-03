0b311ccff68be527907dddec239276be4e58f79e
aniket2727 <aniketkadam5950@gmail.com>
2025-01-03 16:09:55 +0530

    add code

--- app/src/Code232.js
+++ app/src/Code232.js
@@ -2,24 +2,38 @@ import React from 'react'
 
 const Code232 = () => {
 
-   //closure code 
+    //closure code 
 
-   function outer(name){
-       const data="aniket kadam";
-       return  function inner(){
-           console.log("the inner functio is ",data);
-           console.log("name is ",name);
-       }
-   }
+    function outer(name) {
+        const data = "aniket kadam";
+        return function inner() {
+            console.log("the inner functio is ", data);
+            console.log("name is ", name);
+        }
+    }
 
-   const obj=outer("sanket");
-   obj();
+    const obj = outer("sanket");
+    obj();
 
-  return (
-    <div>
-      <h1>code is running</h1>
-    </div>
-  )
+
+    //cuuuring 
+
+
+    function currring(a) {
+        return function (b) {
+            return function (c) {
+                console.log(a + b + c);
+            }
+        }
+    }
+
+    currring(10)(20)(30);
+
+    return (
+        <div>
+            <h1>code is running</h1>
+        </div>
+    )
 }
 
 export default Code232
