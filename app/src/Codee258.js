0a36dc821776fd1a6c27998d16d8bab6e945036e
aniket2727 <aniketkadam5950@gmail.com>
2025-01-16 12:38:48 +0530

    add code

--- app/src/Codee258.js
+++ app/src/Codee258.js
@@ -8,25 +8,39 @@ const Codee258 = () => {
     const[selcteddata,setselecteddata]=useState([]);
 
 
-    let temp={};
+    // let temp={};
     
-     useEffect(()=>{
-        for(let i in data){
-            if(i==='age' ||i==='rollno'){
+    //  useEffect(()=>{
+    //     for(let i in data){
+    //         if(i==='age' ||i==='rollno'){
                   
-                  if(i==='age'){
-                    temp.age=data[i];
-                  }
+    //               if(i==='age'){
+    //                 temp.age=data[i];
+    //               }
     
-                  if(i=='rollno'){
-                    temp.rollno=data[i]
-                  }
-            }
-        }
-        setselecteddata(temp)
-     },[]);
+    //               if(i==='rollno'){
+    //                 temp.rollno=data[i]
+    //               }
+    //         }
+    //     }
+    //     setselecteddata(temp)
+    //  },[]);
 
-     console.log(selcteddata)
+    //  console.log(selcteddata)
+
+
+    // //  const s=data.filter((item)=>{
+    // //      return  item==='age' || item==='rollnumber'});
+    // //  console.log("this is ",s);
+
+
+    useEffect(() => {
+        const { age, rollno } = data; // Destructure the required fields
+        setselecteddata({ age, rollno }); // Update the state with only the required fields
+      }, [data]);
+
+
+      console.log("selcted data ",selcteddata);
  
   return (
     <div>
