2f0a179296f8959c8e976b2224fb7f3d3e289740
aniket2727 <aniketkadam5950@gmail.com>
2025-01-16 13:52:07 +0530

    add code

--- app/src/Code260.js
+++ app/src/Code260.js
@@ -6,14 +6,25 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
 const Code260 = () => {
-    const [data, setdata] = useState({ name: 'aniket', age: '10', roll: '101' });
+    // const [data, setdata] = useState({ name: 'aniket', age: '10', roll: '101' });
 
-    const selected = Object.entries(data).reduce((acc, [key, value]) => {
-        acc[key] = value; // Copy each key-value pair to the accumulator
-        return acc;
-    }, {});
+    // const selected = Object.entries(data).reduce((acc, [key, value]) => {
+    //     acc[key] = value; 
+    //     return acc;
+    // }, {});
 
-    console.log(selected);
+
+    const a=[1,2,3,4,5];
+    const obj=Object.fromEntries(a.map((index,value)=>[index,value]))
+    console.log(obj);
+
+
+    const b=["apple","mango","cake"];
+    const obj1=Object.fromEntries(b.map((index,value)=>[value,index]))
+    console.log(obj);
+    console.log(obj1);
+
+   // console.log(selected);
     return (
         <div>
 
