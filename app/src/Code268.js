e400589d469a2bfd4e7726e0443a85ab9f56e1c8
aniket2727 <aniketkadam5950@gmail.com>
2025-01-22 12:15:02 +0530

    add code

--- app/src/Code268.js
+++ app/src/Code268.js
@@ -1,33 +1,68 @@
 
 
-import React from 'react';
-import {useState}  from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 
 
 
-const HandleDisplayfolder=({folder})=>{
-    console.log("this is folder",folder)
+const HandleDisplayfolder = ({ folder, handlestate }) => {
+  const [subdata, setsubdata] = useState([]);
+
+  useEffect(() => {
+    setsubdata(folder);
+  }, [subdata]);
+
+
+  console.log("this is subdata", subdata);
+
+
+  return (
+    <div style={{position:'relative',marginLeft:'10px'}}>
+      {
+        typeof subdata==='object'?  <button onClick={() => handlestate(folder.name)} style={{ margin: '5px', width: '50px', cursor: 'pointer'}}>{subdata.name}{folder.state?'📂':'📁'}</button>:<p style={{margin:'0px',border:'1px solid red'}}>{subdata}{'📄'}</p>
+      }
+    
+      {
+        subdata?.child?.length > 0 && folder.state===true && subdata.child.map((item, index) => (
+          (<HandleDisplayfolder folder={item} handlestate={handlestate} />)
+        ))
+      }
+    </div>
+  )
+
 }
 
 const Code268 = () => {
 
-    const[data,setdata]=useState([
-       {name:'a',child:[{name:'bb'}]},
-       {name:'b',child:[]},
-       {name:'c',child:[]},
-       'textfile.txt'
-    ]);
+  const [data, setdata] = useState([
+    { name: 'a', child: [{ name: 'aniket' }], state: false },
+    { name: 'b', child: [], state: false },
+    { name: 'c', child: ['aniket.text'], state: false },
+    'textfile.txt'
+  ]);
+
+
+  const handlestate = (root) => {
+    console.log(root)
+
+    const a=data.map((item) => {
+      return item.name === root ? { ...item, state: !item.state } : item
+    })
+    setdata(a);
+  }
+
+ 
   return (
 
     <div>
-        <h1>folder struture</h1>
-        {
-            data.map((item,index)=>{
-               return typeof item==='object'?<div key={index}><HandleDisplayfolder folder={item}/></div>:<div key={index}>{item}</div>
-            })
-        }
-      
+      <h1>folder struture</h1>
+      {
+        data.map((item, index) => {
+          return typeof item === 'object' ? <div key={index}><HandleDisplayfolder folder={item} handlestate={handlestate} /></div> : <div key={index}>{item}</div>
+        })
+      }
+
     </div>
   )
 }
