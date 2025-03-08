be79872bec08adb4fa43477ee0ba5b5c621384bf
aniket2727 <aniketkadam5950@gmail.com>
2025-03-08 13:05:12 +0530

    add code

--- app/src/Code281.js
+++ app/src/Code281.js
@@ -5,23 +5,8 @@ import React from 'react'
 
 const Code281 = () => {
   return (
-    <div>
-
-      
-         <h1>aniket kadam</h1>
-         <h1>added  new code to git repo
-          <h1>added now comments</h1>
-          <h1>add this code to fetaurs </h1>
-
-
-          <h1>this is fourth comment</h1>
-           
-
-           <h1>the code is here</h1>
-
-
-           <h1>this code is from aniket branhc</h1>
-         </h1>
+    <div>    
+        <h1>new changes into the code</h1>
     </div>
   )
 }
