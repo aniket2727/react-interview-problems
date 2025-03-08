264691196ab6c6ec59f2a01ea9fa921225a89825
aniket2727 <aniketkadam5950@gmail.com>
2025-03-08 17:49:21 +0530

    add code

--- app/src/Code301.js
+++ app/src/Code301.js
@@ -4,7 +4,11 @@ const Code301 = () => {
   return (
     <div>
       <h1>Add code by aniket kadam</h1>
+<<<<<<< Updated upstream
       <h1>this code is added by adesh pai</h1>
+=======
+      <h1>this line of code react by sanket </h1>
+>>>>>>> Stashed changes
     </div>
   )
 }
